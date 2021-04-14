465ad6fe99ed41942490e2f2368641a30856f96d
Lee Cheng Hui <chlee9926@gmail.com>
2021-04-14 21:53:35 +0800

    Display uploaded image

--- serve.py
+++ serve.py
@@ -5,33 +5,39 @@ import os
 from werkzeug.utils import secure_filename
 
 
-UPLOAD_FOLDER = 'images'
+UPLOAD_FOLDER = 'static/uploads/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('upload.html')
+
 
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods = ['GET', 'POST'])
+@app.route('/', methods = ['POST'])
 def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        if f.filename == '':
-            flash("Please select a file")
-            return render_template('index.html')
-        elif not allowed_file(f.filename):
-            flash("Invalid file type")
-            return render_template('index.html')
-        f.save(os.path.join(UPLOAD_FOLDER, secure_filename(f.filename)))
-        return 'file uploaded successfully'
+    f = request.files['file']
+    if f.filename == '':
+        flash("Please select a file")
+        return redirect(request.url)
+    elif not allowed_file(f.filename):
+        flash("Invalid file type")
+        return redirect(request.url)
+    filename = secure_filename(f.filename)
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    flash("Upload successfully.")
+    return render_template('upload.html', filename=filename)
     
+@app.route('/display/<filename>')
+def display_image(filename):
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
