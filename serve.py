469de68efc92e9f961013021215700956122c0f7
Lee Cheng Hui <chlee9926@gmail.com>
2021-04-14 21:19:31 +0800

    Upload file feature

--- serve.py
+++ serve.py
@@ -1,12 +1,37 @@
 # Main entry point
 # Start server here
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = 'images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '12345'
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        if f.filename == '':
+            flash("Please select a file")
+            return render_template('index.html')
+        elif not allowed_file(f.filename):
+            flash("Invalid file type")
+            return render_template('index.html')
+        f.save(os.path.join(UPLOAD_FOLDER, secure_filename(f.filename)))
+        return 'file uploaded successfully'
+    
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host='127.0.0.1', port=8000, debug=True)
